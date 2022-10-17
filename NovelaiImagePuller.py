060bab18aadfcf9e3f94adbf04050d2e2b692702
GpointChen <Gpoint.ghdkghdk@gmail.com>
2022-10-17 16:02:16 +0800

    10/17 Add language supports.

--- NovelaiImagePuller.py
+++ NovelaiImagePuller.py
@@ -22,7 +22,11 @@ my_config = {
     "sampler": "k_euler_ancestral",
     "steps": 28,
     "ucPreset": 0,
-    "uc": "lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry"
+    "uc": ("lowres, bad anatomy, bad hands, text, error, missing fingers, "
+           "extra digit, fewer digits, cropped, worst quality, low quality, "
+           "normal quality, jpeg artifacts, signature, watermark, username, "
+           "blurry"),
+    "language": "zh_TW"
 }
 resolutions = ["Portrait (Normal): 512x768",
                "Landscape (Normal): 768x512",
@@ -38,10 +42,45 @@ models = {
 samplers = ['k_euler_ancestral', 'k_euler', 'k_lms', 'plms', 'ddim']
 URL = "https://api.novelai.net/ai/generate-image"
 key = "Put your key here"
-global_save_path = "download\\"
+global_save_path = "download"
+key_path = os.path.join('config', 'key.json')
+setting_path = os.path.join('config', 'settings.json')
+local = {}
+local_files = []
 
 
 def gui():
+    def get_local_files():
+        global local_files
+        local_path = os.path.join('config', 'language')
+        local_files = [f.replace('.json', '') for f in os.listdir(
+            local_path) if os.path.isfile(os.path.join(local_path, f))]
+
+    def get_local(*args):
+        try:
+            with open(os.path.join('config', 'language', lang_var.get() + '.json'), 'r', encoding='UTF-8') as f:
+                global local
+                local = json.load(f)
+
+            lang_label.config(text=local['label_language'])
+            input_label.config(text=local['label_prompt'])
+            input_cb.config(text=local['button_clear'])
+            uc_label.config(text=local['label_uc'])
+            uc_cb.config(text=local['button_clear'])
+            num_label.config(text=local['label_amount'])
+            resol_label.config(text=local['label_resolution'])
+            model_label.config(text=local['label_model'])
+            scale_label.config(text=local['label_scale'])
+            steps_label.config(text=local['label_steps'])
+            sampler_label.config(text=local['label_sampler'])
+            key_label.config(text=local['label_key'])
+            save_bt.config(text=local['button_save'])
+            down_bt.config(text=local['button_download'])
+            open_bt.config(text=local['button_open'])
+        except Exception as e:
+            print(e)
+            logging.error(e)
+
     def save():
         global key, my_config
         try:
@@ -54,23 +93,24 @@ def gui():
                 "sampler": sampler_var.get(),
                 "steps": int(steps_var.get()),
                 "ucPreset": 0,
-                "uc": uc_entry.get("1.0", 'end-1c')
+                "uc": uc_entry.get("1.0", 'end-1c'),
+                "language": lang_var.get()
             }
-            with open('settings.json', 'w', encoding='UTF-8') as f:
+            with open(setting_path, 'w', encoding='UTF-8') as f:
                 json.dump(my_config, f, indent=4)
             key = key_entry.get()
-            with open('key.json', 'w', encoding='UTF-8') as f:
+            with open(key_path, 'w', encoding='UTF-8') as f:
                 json.dump({"key": key}, f, indent=4)
-            logging.info("Config saved in settings.json.")
+            logging.info("Config saved in settings.json and key.json.")
 
         except Exception as e:
             logging.exception(e)
-            progress.config(text="錯誤發生。儲存失敗。")
+            progress.config(text=local["error_save"])
 
     def process(n, p_config):
         global key, global_save_path
         if not key:
-            progress.config(text="沒有驗證金鑰。下載失敗。")
+            progress.config(text=local["error_key"])
             logging.warning("No key detected. Please check your key.json.")
             return None
 
@@ -104,11 +144,11 @@ def gui():
 
             # make dir
             current_time = datetime.now().strftime('%Y-%m-%d %H-%M-%S')
-            global_save_path = "download\\%s\\" % current_time
+            global_save_path = os.path.join("download", current_time)
             save_path = global_save_path
             if not os.path.isdir(save_path):
                 os.mkdir(save_path)
-            shutil.copy('settings.json', save_path + 'settings.json')
+            shutil.copy(setting_path, os.path.join(save_path, 'settings.json'))
             success = 0
             logging.info('Save path: %s' % save_path)
             logging.info('parameters: %s' % json.dumps(my_data))
@@ -117,27 +157,26 @@ def gui():
                 seed = math.floor(random.random() * 4294967296)
                 my_data["parameters"]["seed"] = seed
                 filename = "ID-%04d-SEED-%d.png" % (i, seed)
-                now_at = "%s%s (%d/%d)" % (save_path, filename, i, n)
+                lfn = os.path.join(save_path, filename)
+                now_at = "%s (%d/%d)" % (lfn, i, n)
                 progress.config(text=now_at)
                 logging.info(now_at)
 
                 # get the file
                 r = requests.post(url=URL, headers=my_headers, json=my_data)
                 if r.status_code == requests.codes.created:
-                    with open(save_path + filename, 'wb') as out_file:
-                        data = r.text.replace(
-                            "event: newImage\nid: 1\ndata:", "")
-                        out_file.write(base64.b64decode(data))
+                    with open(lfn, 'wb') as outf:
+                        outf.write(base64.b64decode(r.text.replace(
+                            "event: newImage\nid: 1\ndata:", "")))
                     success += 1
                 else:
                     logging.warning("Download failed: %s" % r.text)
-                    progress.config(text="%s%s (%d/%d) 下載失敗。" % (
-                        save_path, filename, i, n))
+                    progress.config(text=local["error_file"] % (lfn, i, n))
 
-            progress.config(text="%d張圖片下載完成。" % success)
+            progress.config(text=local["after_download"] % success)
         except Exception as e:
             logging.exception(e)
-            progress.config(text="錯誤發生。下載失敗。")
+            progress.config(text=local["error_download"])
 
     def download(num):
         global my_config
@@ -163,84 +202,104 @@ def gui():
         return True
 
     global key
-    with open('settings.json') as f:
+    with open(setting_path) as f:
         config = json.load(f)
-    with open('key.json') as f:
+    with open(key_path) as f:
         key = json.load(f)['key']
+    get_local_files()
     window = tk.Tk()
-    window.title('NovelAI Image Piliang Spawner')
+    window.title('NovelAI Image Puller')
 
     frames = [tk.Frame(window) for i in range(4)]
     for fi in frames:
         fi.pack(anchor="w", padx=5, pady=1)
 
-    tk.Label(frames[0], text="關鍵字", width=10).grid(column=0, row=0)
-    input_entry = tk.Text(frames[0], width=64, height=4, font=('Arial'))
+    lang_label = tk.Label(frames[0], width=10)
+    lang_label.pack(side=tk.LEFT)
+    lang_var = tk.StringVar(frames[0])
+    lang_var.set(config['language'])
+    lang_var.trace_add('write', get_local)
+    lang_entry = tk.OptionMenu(frames[0], lang_var, *local_files)
+    lang_entry.configure(width=25)
+    lang_entry.pack(side=tk.LEFT)
+
+    input_label = tk.Label(frames[1], width=10)
+    input_label.grid(column=0, row=0)
+    input_entry = tk.Text(frames[1], width=64, height=4, font=('Arial'))
     input_entry.insert(1.0, config['input'])
     input_entry.grid(column=1, row=0, sticky='ew')
-    tk.Button(frames[0], text="清除",
-              command=lambda: input_clear(input_entry)).grid(column=2, row=0)
+    input_cb = tk.Button(frames[1], command=lambda: input_clear(input_entry))
+    input_cb.grid(column=2, row=0)
 
-    tk.Label(frames[0], text="黑名單", width=10).grid(column=0, row=1)
-    uc_entry = tk.Text(frames[0], width=64, height=2, font=('Arial', 10))
+    uc_label = tk.Label(frames[1], width=10)
+    uc_label.grid(column=0, row=1)
+    uc_entry = tk.Text(frames[1], width=64, height=2, font=('Arial', 10))
     uc_entry.insert(1.0, config['uc'])
     uc_entry.grid(column=1, row=1, sticky='ew')
-    tk.Button(frames[0], text="清除",
-              command=lambda: input_clear(uc_entry)).grid(column=2, row=1)
+    uc_cb = tk.Button(frames[1], command=lambda: input_clear(uc_entry))
+    uc_cb.grid(column=2, row=1)
 
-    tk.Label(frames[2], text="數量", width=10).grid(column=0, row=0)
+    num_label = tk.Label(frames[2], width=10)
+    num_label.grid(column=0, row=0)
     num_var = tk.StringVar(frames[2], config['number'])
     num_entry = tk.Entry(frames[2], textvariable=num_var, width=25)
     num_entry.bind("<FocusOut>", lambda x: check_num(num_var, 1, 1000))
     num_entry.grid(column=1, row=0)
 
-    tk.Label(frames[2], text="解析度", width=10).grid(column=0, row=1)
+    resol_label = tk.Label(frames[2], width=10)
+    resol_label.grid(column=0, row=1)
     resol_var = tk.StringVar(frames[2])
     resol_var.set(config['resolution'])
     resol_entry = tk.OptionMenu(frames[2], resol_var, *resolutions)
     resol_entry.configure(width=25)
     resol_entry.grid(column=1, row=1)
 
-    tk.Label(frames[2], text="模型", width=10).grid(column=2, row=1)
+    model_label = tk.Label(frames[2], width=10)
+    model_label.grid(column=2, row=1)
     model_var = tk.StringVar(frames[2])
     model_var.set(config['model'])
     model_entry = tk.OptionMenu(frames[2], model_var, *list(models.keys()))
     model_entry.configure(width=25)
     model_entry.grid(column=3, row=1)
 
-    tk.Label(frames[2], text="Steps", width=10).grid(column=0, row=2)
+    steps_label = tk.Label(frames[2], width=10)
+    steps_label.grid(column=0, row=2)
     steps_var = tk.StringVar(frames[2], config['steps'])
     steps_entry = tk.Entry(frames[2], textvariable=steps_var, width=25)
     steps_entry.bind("<FocusOut>", lambda x: check_num(steps_var, 2, 28))
     steps_entry.grid(column=1, row=2)
 
-    tk.Label(frames[2], text="Scale", width=10).grid(column=2, row=2)
+    scale_label = tk.Label(frames[2], width=10)
+    scale_label.grid(column=2, row=2)
     scale_var = tk.StringVar(frames[2], config['scale'])
     scale_entry = tk.Entry(frames[2], textvariable=scale_var, width=25)
     scale_entry.bind("<FocusOut>", lambda x: check_num(scale_var, 2, 100))
     scale_entry.grid(column=3, row=2)
 
-    tk.Label(frames[2], text="進階：取樣", width=10).grid(column=0, row=3)
+    sampler_label = tk.Label(frames[2], width=10)
+    sampler_label.grid(column=0, row=3)
     sampler_var = tk.StringVar(frames[2])
     sampler_var.set(config['sampler'])
     sampler_entry = tk.OptionMenu(frames[2], sampler_var, *samplers)
     sampler_entry.configure(width=25)
     sampler_entry.grid(column=1, row=3)
 
-    tk.Label(frames[2], text="認證金鑰", width=10).grid(column=2, row=3)
+    key_label = tk.Label(frames[2], width=10)
+    key_label.grid(column=2, row=3)
     key_entry = tk.Entry(frames[2], width=25)
     key_entry.insert(0, key)
     key_entry.grid(column=3, row=3)
 
-    tk.Button(frames[3], text="儲存設定", width=10,
-              command=save).pack(side=tk.LEFT)
-    tk.Button(frames[3], text="產生圖片", width=10,
-              command=lambda: threading.Thread(target=lambda: download(
-                  int(num_entry.get()))).start()).pack(side=tk.LEFT)
-    tk.Button(frames[3], text="開啟圖片", width=10,
-              command=open_folder).pack(side=tk.LEFT)
+    save_bt = tk.Button(frames[3], text="儲存設定", width=10, command=save)
+    save_bt.pack(side=tk.LEFT)
+    down_bt = tk.Button(frames[3], text="產生圖片", width=10, command=lambda: threading.Thread(target=lambda: download(int(num_entry.get()))).start())
+    down_bt.pack(side=tk.LEFT)
+    open_bt = tk.Button(frames[3], text="開啟圖片", width=10, command=open_folder)
+    open_bt.pack(side=tk.LEFT)
     progress = tk.Label(frames[3], text='', width=60, anchor='w')
     progress.pack(side=tk.LEFT)
+
+    get_local()
 
     window.eval('tk::PlaceWindow . center')
     window.mainloop()
@@ -248,14 +307,15 @@ def gui():
 
 if __name__ == "__main__":
     try:
-        if not os.path.exists('key.json'):
-            with open('key.json', 'w', encoding='UTF-8') as f:
+        for d in ['config', 'download']:
+            if not os.path.isdir(d):
+                os.mkdir(d)
+        if not os.path.exists(key_path):
+            with open(key_path, 'w', encoding='UTF-8') as f:
                 json.dump({"key": key}, f, indent=4)
-        if not os.path.exists('settings.json'):
-            with open('settings.json', 'w', encoding='UTF-8') as f:
+        if not os.path.exists(setting_path):
+            with open(setting_path, 'w', encoding='UTF-8') as f:
                 json.dump(my_config, f, indent=4)
-        if not os.path.isdir('download'):
-            os.mkdir('download')
 
         # log setup
         logging.basicConfig(filename='nips.log', level=logging.DEBUG,
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # NovelAI Image Puller (NIP)
 A tool to batch download the images produced by NovelAI by its API.
 
-Before start downloading, you may need to fill in the key in `key.json` like below:
+Before start downloading, you may need to fill in the key in `config\key.json` like below:
 
 ```json
 {
@@ -16,3 +16,5 @@ All your settings will be saved automatically when you start requesting images, 
 If you mess up your settings, you can delete `settings.json` and it will be re-generated when launch the app.
 
 You can check the log in `nips.log`.
+
+* For older version before 0.2, all json files are stored in the same folder as the exe file.
